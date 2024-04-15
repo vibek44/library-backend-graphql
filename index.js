03d16cff18db9e777203351356b656841faf81f6
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-15 15:54:09 +0300

    passing context function to resolve the authentication process and result derived is shared with resolvers

--- index.js
+++ index.js
@@ -1,217 +1,16 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
-const { v1: uuid } = require('uuid')
-const { GraphQLError } = require('graphql')
+const mongoose = require('mongoose')
+require('dotenv').config()
+const resolvers = require('./resolver')
+const typeDefs = require('./typedef')
+const jwt = require('jsonwebtoken')
+const User = require('./models/user')
 
-let authors = [
-  {
-    name: 'Robert Martin',
-    id: 'afa51ab0-344d-11e9-a414-719c6709cf3e',
-    born: 1952,
-  },
-  {
-    name: 'Martin Fowler',
-    id: 'afa5b6f0-344d-11e9-a414-719c6709cf3e',
-    born: 1963,
-  },
-  {
-    name: 'Fyodor Dostoevsky',
-    id: 'afa5b6f1-344d-11e9-a414-719c6709cf3e',
-    born: 1821,
-  },
-  {
-    name: 'Joshua Kerievsky', // birthyear not known
-    id: 'afa5b6f2-344d-11e9-a414-719c6709cf3e',
-  },
-  {
-    name: 'Sandi Metz', // birthyear not known
-    id: 'afa5b6f3-344d-11e9-a414-719c6709cf3e',
-  },
-]
-
-/*
- * It might make more sense to associate a book with its author by storing the author's id in the context of the book instead of the author's name
- * However, for simplicity, we will store the author's name in connection with the book
- */
-
-let books = [
-  {
-    title: 'Clean Code',
-    published: 2008,
-    author: 'Robert Martin',
-    id: 'afa5b6f4-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Agile software development',
-    published: 2002,
-    author: 'Robert Martin',
-    id: 'afa5b6f5-344d-11e9-a414-719c6709cf3e',
-    genres: ['agile', 'patterns', 'design'],
-  },
-  {
-    title: 'Refactoring, edition 2',
-    published: 2018,
-    author: 'Martin Fowler',
-    id: 'afa5de00-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring'],
-  },
-  {
-    title: 'Refactoring to patterns',
-    published: 2008,
-    author: 'Joshua Kerievsky',
-    id: 'afa5de01-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'patterns'],
-  },
-  {
-    title: 'Practical Object-Oriented Design, An Agile Primer Using Ruby',
-    published: 2012,
-    author: 'Sandi Metz',
-    id: 'afa5de02-344d-11e9-a414-719c6709cf3e',
-    genres: ['refactoring', 'design'],
-  },
-  {
-    title: 'Crime and punishment',
-    published: 1866,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de03-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'crime'],
-  },
-  {
-    title: 'The Demon ',
-    published: 1872,
-    author: 'Fyodor Dostoevsky',
-    id: 'afa5de04-344d-11e9-a414-719c6709cf3e',
-    genres: ['classic', 'revolution'],
-  },
-]
-
-const typeDefs = `
-type Book{
-  title:String!
-  author:String!
-  published:Int!
-  genres:[String!]!
-}
-
-type Author{
-  name:String!
-  id:ID!
-  born:Int
-  bookCount:Int!
-  
-}
-  type Query {
-    bookCount: Int
-    authorCount:Int
-    allBooks(author:String genre:String):[Book!]!
-    allAuthors:[Author!]!
-  }
-
-  type Mutation{
-    addBook(
-      title:String!
-      author:String!
-      published:Int!
-      genres:[String!]!
-      ):Book
-    addAuthor(
-      name:String!
-      born:Int
-    ):Author
-    editAuthor(
-      name:String!
-      born:Int!
-    ):Author
-   
-  }
-`
-
-const resolvers = {
-  Query: {
-    bookCount: () => books.length,
-    authorCount: () => authors.length,
-    allBooks: (root, args) => {
-      if (args.author) {
-        let authorBooks = books.filter((book) => book.author === args.author)
-        return authorBooks
-      }
-      if (args.genre) {
-        let genreBooks = books.filter((book) => {
-          const decision = book.genres.map((el) => {
-            if (el === args.genre) return true
-            return false
-          })
-          //console.log(decision)
-          return decision.includes(true)
-        })
-        return genreBooks
-      }
-      return books
-    },
-    allAuthors: (root, args) => {
-      const changedAuthors = authors.map((author) => {
-        const changedBooks = books.filter((book) => book.author === author.name)
-        return {
-          ...author,
-          bookCount: changedBooks.length,
-        }
-      })
-      return changedAuthors
-    },
-  },
-  Mutation: {
-    addBook: (root, args) => {
-      console.log(args);
-      
-      if (
-        books.find(
-          (book) => book.title.toLowerCase() === args.title.toLowerCase()
-        )
-      ) {
-        throw new GraphQLError('Title must be unique', {
-          extensions: {
-            code: 'BAD_TITLE_INPUT',
-            invalidArgs: args.title,
-          },
-        })
-      }
-      const book = { ...args, id: uuid() }
-      books = books.concat(book)
-      return book
-    },
-    addAuthor: (root, args) => {
-      if (
-        authors.find(
-          (author) => author.name.toLowerCase() === args.name.toLowerCase()
-        )
-      ) {
-        throw new GraphQLError('name must be unique', {
-          extensions: {
-            code: 'BAD_TITLE_INPUT',
-            invalidArgs: args.name,
-          },
-        })
-      }
-      const author = { ...args, id: uuid() }
-      authors = authors.concat(author)
-      return author
-    },
-    editAuthor: (root, args) => {
-      const author = authors.find(
-        (author) => author.name.toLowerCase() === args.name.toLowerCase()
-      )
-      let updatedAuthor = { ...author, born: args.born }
-
-      authors = authors.map((author) =>
-        author.name.toLowerCase() === updatedAuthor.name.toLowerCase()
-          ? updatedAuthor
-          : author
-      )
-      return updatedAuthor
-    },
-  },
-}
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log('connected'))
+  .catch(() => console.log('error mongodb'))
 
 const server = new ApolloServer({
   typeDefs,
@@ -220,6 +19,16 @@ const server = new ApolloServer({
 
 startStandaloneServer(server, {
   listen: { port: 4000 },
+  //context is passed to all resolver of query and mutation as parameter
+  context: async ({ req, res }) => {
+    const auth = req ? req.headers.authorization : null
+
+    if (auth && auth.startsWith('bearer')) {
+      const decodedToken = jwt.verify(auth.substring(7), process.env.JWT_SEKRET)
+      const currentUser = await User.findById(decodedToken.id)
+      return { currentUser }
+    }
+  },
 }).then(({ url }) => {
   console.log(`Server ready at ${url}`)
 })
