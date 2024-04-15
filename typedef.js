d7d124ad4adbb1a7dedeb99ff4ab8ba34671934c
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-04-15 15:57:22 +0300

    adding graphql schema for type User,Token and adding me query along with creating mutation createuser and login

--- typedef.js
+++ typedef.js
@@ -1,9 +1,12 @@
 const typeDefs = `
-type Book{
-  title:String!
-  author:String!
-  published:Int!
-  genres:[String!]!
+type User{
+  username:String!
+  favoriteGenre:String!
+  id:ID!
+}
+
+type Token{
+  value:String!
 }
 
 type Author{
@@ -13,20 +16,37 @@ type Author{
   bookCount:Int!
   
 }
-  type Query {
+
+type Book{
+  title:String!
+  author:Author!
+  published:Int!
+  genres:[String!]!
+}
+
+type Query {
+    me:User
     bookCount: Int
     authorCount:Int
     allBooks(author:String genre:String):[Book!]! 
     allAuthors:[Author!]!
   }
 
-  type Mutation{
+type Mutation{
+    createUser(
+      username:String!
+      favoriteGenre:String!
+      ):User
+    login(
+      username:String!
+      password:String!
+    ):Token
     addBook(
       title:String!
       author:String!
       published:Int!
       genres:[String!]!
-      ):Book
+      ):Book!
     addAuthor(
       name:String!
       born:Int
@@ -39,4 +59,4 @@ type Author{
   }
 `
 
-module.exports=typeDefs
+module.exports = typeDefs
